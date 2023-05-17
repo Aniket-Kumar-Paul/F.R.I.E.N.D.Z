0c2926d9a6f99d60f904aa0f731d656b548f5868
Aniket <aniketkumarpaul1@gmail.com>
2023-05-17 12:26:47 +0530

    Styling Setup, Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,31 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
 
 function App() {
-
+  const mode = useSelector((state) => state.mode); // grab from initialstate
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className="app">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
-      </Routes>
+        <ThemeProvider theme={theme}>
+          {/* <CssBaseline /> is used as a foundational component to normalize or 
+        reset default browser styles, providing a consistent starting point for 
+        styling web applications in Material-UI or similar frameworks */}
+          <CssBaseline />
+
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
